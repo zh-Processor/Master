ae4bb535747398d868e09b4717ee466060f5fd15
zh-Processor <processorlee@gmail.com>
2017-12-12 01:01:25 +0800

    beta

--- master.py
+++ master.py
@@ -6,8 +6,9 @@ import time
 import logging
 import threading
 import random
-from core.log import logger
-from core.log import log
+from log import logger
+from log import log
+from pwn import *
 
 iplist = ['127.0.0.1']
 name = ['web1' , 'web2' , 'pwn1' , 'pwn2']
@@ -15,10 +16,41 @@ port = 80
 round_time = 60
 timeout = 3
 Retrys = 2
+max_round = 100
 
-bool ok
+def sha256(str):
+    m = hashlib.sha256()   
+    m.update(str)
+    return m.hexdigest()
 
-def run(ip):
+def connect(ip):
+    ip = "'" + ip + "'"
+    flag = "hctf{" + sha256(round + "f33601c10397b25e789c85bce5f2fbfd") + "}"
+    payload = 'echo ' + '"' + flag + '" ' + '> ' + 'flag.txt'
+    logger.info("Start connect %s" % ip)
+    try:
+        p = remote(ip , port)
+    except requests.exceptions.ConnectTimeout:
+        logger.warn("Time out !!!")
+        continue
+    except:
+        logger.warn("Unexpected Error")
+        continue
+
+    logger.info("Start flush %s" % ip)
+    try:
+        p.sendline(payload)
+    except requests.exceptions.ConnectTimeout:
+        logger.warn("Time out !!!")
+        continue
+    except IOError:
+        logger.warn("IOError!")
+        continue
+    except:
+        logger.warn("Unexpected Error")
+        continue
+
+def check(ip):
     logger.info("Start check %s" %ip)
     for i in xrange(Retrys):
         try:
@@ -44,4 +76,42 @@ def run(ip):
             continue
 
 def main():
-    
+    while 1:
+        round = getround()
+        old_round = round
+        if round != old_round:
+            logger.info("Start %d round" % round)
+            logger.info("This round time is %s" % time.strftime('%H:%M:%S',time.localtime(time.time())))
+            threads_connect = []
+            for ip in iplist:
+        		try:
+        			w = threading.Thread(target=connect, name='thread for %s' % ip, args=(ip,))
+        			w.start()
+        			threads_connect.append(w)
+        		except:
+        			logger.error("Thread error...")
+
+            for w in threads_connect:
+                w.join()
+
+            threads_check = []
+            delay = random.randint(10,180)
+            time.sleep(delay)
+            for ip in iplist:
+    			try:
+    				t = threading.Thread(target=check, name='thread for %s' % ip, args=(ip,))
+    				t.start()
+    				threads_check.append(t)
+    			except:
+    				logger.error("Thread error...")
+
+            for t in threads_check:
+    			t.join()
+
+            logger.info("Round %s is finished" % round)
+            if round >= max_round:
+    			logger.warn("All round finished...")
+    			break
+
+if __name__ == '__main__':
+    main()
