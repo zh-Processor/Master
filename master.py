13fa5b9b6f28003576659c823d5566faf5595940
zh-Processor <processorlee@gmail.com>
2017-12-14 16:16:52 +0800

    beta

--- master.py
+++ master.py
@@ -17,32 +17,34 @@ import json
 #timeout：超时时间
 #Retrys：重试次数
 #team0:{name , pwn1 , pwn2 , web1 , web2 , sql_username , sql_password , pwn1_down , pwn2_down , web1_down , web2_down}
-#line 53 , line 75 需添加地址
 
-
-max_rank = 2000
-mul = 1.2
 per_rank = 500
 datebase = ''
 port = 80
 timeout = 3
 Retrys = 2
 max_round = 100
-team0 = ['vidar' , '127.0.0.1' , '127.0.0.1' ,'127.0.0.1' ,'127.0.0.1' ,'username' , 'password' , 0 , 0 , 0 , 0]
+team0 = ['vidar' , '127.0.0.1' , '127.0.0.1' ,'127.0.0.1' ,'127.0.0.1' , '127.0.0.1' ,'username' , 'password']
 
+round = 0
 token = 'mDktXt32gPdO9C*4G%JO*nMi^9C7$mzR'
-name = ['web1' , 'web2' , 'pwn1' , 'pwn2']
+name = ['HYPERION' , 'BIGBROTHER' , 'DELIVER', 'pwn1' , 'pwn2']
 team = [team0 ,team1 ,team2 ,team3 ,team4 ,team5 ,team6 ,team7 ,team8 ,team9]
 pwn1list = []
 pwn2list = []
 web1list = []
 web2list = []
-iplist = pwn1list + pwn2list + web1list + web2list
+web3list = []
+
 for index in range(len(team)):
     pwn1list.append(team[index][1])
     pwn2list.append(team[index][2])
     web1list.append(team[index][3])
     web2list.append(team[index][4])
+    web3list.append(team[index][5])
+
+
+iplist = pwn1list + pwn2list + web1list + web2list + web3list
 
 def sha256(str):
     m = hashlib.sha256()   
@@ -51,18 +53,14 @@ def sha256(str):
 
 def rank(teamname):
     inc = per_rank
-    for i in range(down_round):
-        inc = inc * mul
-    if inc >= max_rank:
-        inc = max_rank
     for j in range(Retrys):
         try:
             s = requests.Session()
-            data = {"teamName": teamname, "inc": inc}
-            r = s.post("http://xxxx/Team/incScore",data = data)
+            data = {"teamName": teamname, "inc": inc ,"token": token}
+            r = s.post("http://192.168.1.107:3000/Team/incScore",data = data)
             if r.status_code == 200:
                 content = r.content
-                logger.info("[Rank] Post rank sucess")
+                logger.info("[Rank] Post rank success")
             else:
                 logger.error("[Rank] Post rank failure")
         except requests.exceptions.ConnectTimeout:
@@ -77,10 +75,10 @@ def getround():
     for i in range(Retrys):
         try:
             s = requests.Session()
-            r = s.get("http://xxxx/System/info")
+            r = s.get("http://192.168.1.107:3000/System/info")
             if r.status_code == 200:
                 content = r.content
-                logger.info("[ROUND] Get round sucess")
+                logger.info("[ROUND] Get round success")
             else:
                 logger.error("[ROUND] Get round failure")
         except requests.exceptions.ConnectTimeout:
@@ -99,7 +97,7 @@ def buff(team_name,challenge_name):
         try:
             s = requests.Session()
             data = {"teamName": team_name, "challengeName": challenge_name, "status": status, "token": token}
-            r = s.post("http://xxxx/System/setServerStatus", data=data)
+            r = s.post("http://192.168.1.107:3000/System/setServerStatus", data=data)
             if r.status_code == 200:
                 content = r.content
                 logger.info("[POST] Post buff sucess")
@@ -118,16 +116,14 @@ def connectip(ip):
 
     if ip in pwn1list:
         challenge_name = "pwn1"
-        challenge_port = pwn1_port
     elif ip in pwn2list:
         challenge_name = "pwn2"
-        challenge_port = pwn2_port
     elif ip in web1list:
-        challenge_name = "web1"
-        challenge_port = web1_port
+        challenge_name = "HYPERION"
+    elif ip in web2list:
+        challenge_name = "DELIVER"
     else:
-        challenge_name = "web2"
-        challenge_port = web2_port
+        challenge_name = "BIGBROTHER"
 
     for index in range(len(team)):
         if ip in team[index]:
@@ -150,7 +146,7 @@ def connectip(ip):
             continue
 
         logger.info("Start flush %s" % ip)
-        if ip in pwn1list or ip in pwn2list:
+        if ip in pwn1list or ip in pwn2list or ip in web1list or ip in web2list:
             payload = 'echo ' + '"' + flag + '" ' + '> ' + 'flag.txt'
             try:
                 p.sendline(payload)
@@ -167,7 +163,7 @@ def connectip(ip):
                 continue
         else:
             query = "update {database} set flag={flag} where xxxxxx".format(database=datebase, flag=flag)
-            payload = "mysql -u {username} -p {password} -e \'{query}\'".format(username=now_team[5], password=now_team[6], query=query)
+            payload = "mysql -u {username} -p {password} -e \'{query}\'".format(username=now_team[6], password=now_team[7], query=query)
             try:
                 p.sendline(payload)
                 break
@@ -193,32 +189,29 @@ def check(ip):
         try:
             if ip in web1list:
                 ok = webcheck1(ip)
-                name_1 = 'web1'
-                index_1 = 9
+                name_1 = 'HYPERION'
             elif ip in web2list:
                 ok = webcheck2(ip)
-                name_1 = 'web2'
-                index_1 = 10
+                name_1 = 'DELIVER'
+            elif ip in web3list:
+                ok = webcheck3(ip)
+                name_1 = 'BIGBROTHER'
             elif ip in pwn1list:
                 ok = pwncheck1(ip)
                 name_1 = 'pwn1'
-                index_1 = 7
             elif ip in pwn2list:
                 ok = pwncheck2(ip)
                 name_1 = 'pwn2'
-                index_1 = 8
             
             if ok == False:
                 logger.warn("[CHECK] [{}] Check {} Failure" .format(ip , name_1))
                 status = 'down'
                 buff(now_team , name[index] , status) 
-                team[index][index_1] += 1
-                rank(team[index][0])
             else:
                 logger.debug("[CHECK] [{}] Check {} Success".format(ip , name_1))
                 status = 'up'
                 buff(now_team , name[index] , status)
-                team[index][index_1] = 0
+
         except:
             logger.warning("[CHECK] [{}] [{}] Error....".format(ip , name_1))
             traceback.print_exc()
